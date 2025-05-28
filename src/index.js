46a80c1c07819330ffd7505224de14931b8ef069
Yusein Shabu <iusein2001shabu@abv.bg>
2025-05-28 10:46:14 +0300

    Add Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('work !');
+import express from 'express' ;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It work !');
+    res.end();
+});
+
+app.listen(5000, () => console.log('Server listening on http://localhost:5000 ... '))
