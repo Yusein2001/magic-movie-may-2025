15d5154e147f7b6365f2091c22bdbd371034ee58
Yusein Shabu <iusein2001shabu@abv.bg>
2025-06-02 13:03:39 +0300

    Fix main.hbs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,16 +5,16 @@ const homeController = express.Router();
 
 homeController.get('/', async (req, res) => {
     const movieArr = await movieServices.getAll();
-    res.render('home', { movieArr });
+    res.render('home', { movieArr, pageTitle: "Catalog Page", imgSrc: "/img/logo.webp" });
 });
 
 
 homeController.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { pageTitle: "About Page", imgSrc: "/img/logo.webp" });
 });
 
 homeController.get('/create', (req, res) => {
-    res.render('createMovie');
+    res.render('createMovie', { pageTitle: "Create Page", imgSrc: "https://cdn.pixabay.com/photo/2013/07/13/13/36/film-161204_1280.png" });
 });
 
 export default homeController ;
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -5,14 +5,25 @@ import { movieServices } from '../services/movieServices.js';
 const movieController = express.Router();
 
 movieController.post('/addMovie', async (req, res) => {
+
     const urlData = req.body ;
+
     const id = Date.now();
     urlData.id = id;
+
     const movieBaseData = await movieServices.getAll();
     movieBaseData.push(urlData);
-    console.log(movieBaseData);
+
     await movieServices.add(movieBaseData);
-    res.end();
+
+    res.redirect('/');
 })
+
+movieController.get('/:id/details', (req, res) => {
+    res.render('details');
+})
+
+
+
 
 export default movieController ;
